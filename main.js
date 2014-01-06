4d5e442aa17f99b036725933336b3697badc118f
scott1028 <mic1028002@gmail.com>
2014-01-06 08:01:06 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+// 參考：http://tw.knowledge.yahoo.com/question/question?qid=1507100703097
+// 盒內塔遞迴移動盤子範例
+
+// function(盤子數, 來源柱編號, 緩衝柱編號, 目標柱編號)
+var move=function(n,source_stack,temp_stack,dest_stack){
+	if(n==1){
+		console.log('from '+source_stack.toString()+' move to '+dest_stack.toString()+' !');
+	}
+	else if(n>1){
+		// 將 n-1 個盤子先移到 temp 柱子上
+		move(n-1,source_stack,dest_stack,temp_stack);
+		// 將 source 盤最下方的一道 destination
+		console.log('from '+source_stack.toString()+' move to '+dest_stack.toString()+' #');
+		// 將 temp 盤子一道 destination 柱子上
+		move(n-1,temp_stack,source_stack,dest_stack);
+	}
+};
+
+
+move(3,1,2,3);
+
+/*
+	演算法原理：
+		如果要將 n 個盤子從 source_stack 移動到 dest_stack，勢必先將 n-1 個盤子先從 source_stack 移動到 temp_stack，所以至少會有三根柱子。
+		n 盤移動 a to b 的部分演算法是一樣的，假設一個 function 所做的事情就是這件事，並命名為 move。
+
+		簡單來說就是：
+			如果要完成 n 塔從 source_stack 移動到 dest_stack，必定要先完成 n-1 塔從 source_stack 移動到 temp
+			再把 source_stack 塔的最底下那塊移動到 dest_stack，再把 n-1 塔移動到 dest_stack。
+
+		演算法推算
+			1 塔的時候即：
+				source to dest
+
+			2 塔的時候即：
+
+				source to temp(1塔的演算法)
+					...
+				source to dest
+					...
+				temp to dest
+
+			3 塔的時候即：
+
+				source to temp(2塔演算法)
+					...
+				source to dest
+					...
+				temp to dest
+
+			4~n 塔的時候同理。
+				...
+
+			就可以推算出演算法：
+
+				var move=function(n,source_stack,temp_stack,dest_stack){
+					if(n==1){
+						// 如果是一塔的時候
+						console.log('from '+source_stack.toString()+' move to '+dest_stack.toString()+' !');
+					}
+					else if(n>1){
+						// 將 n-1 塔演算法
+						move(n-1,source_stack,dest_stack,temp_stack);
+
+						// 將來盤子從編號為 ? 的來源柱移動到編號為 ? 的目的柱
+						console.log('from '+source_stack.toString()+' move to '+dest_stack.toString()+' #');
+						
+						// 再將先前的 n-1 個盤子從 temp_stack 移動到 dest_stack
+						move(n-1,temp_stack,source_stack,dest_stack);
+					}
+				}
+
+*/
